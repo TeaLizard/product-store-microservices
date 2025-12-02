26a30757bcdb432e3c33b86a9cba2a729ff8ea7b
TeaLizard <owentwilliams7@gmail.com>
2025-12-01 23:17:42 -0500

    updated order entity

--- order-service/src/main/java/com/example/order_service/entity/Order.java
+++ order-service/src/main/java/com/example/order_service/entity/Order.java
@@ -1,6 +1,8 @@
 package com.example.order_service.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 import java.util.List;
 
@@ -16,6 +18,16 @@ public class Order {
     @Column(name = "product_id")
     private List<Integer> productIds;
 
+    @NotNull
+    @NotBlank
+    @Column
+    private String status;
+
+    @NotNull
+    @NotBlank
+    @Column
+    private String location;
+
     public Order() {}
 
     public Integer getId() {
@@ -30,5 +42,19 @@ public class Order {
     }
     public void setShoes(List<Integer> productIds) {
         this.productIds = productIds;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+    public void setLocation(String location) {
+        this.location = location;
     }
 }
